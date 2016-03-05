d3add471573e2b0e9adf9ee2637fe7eb5bb85146
Daniel Senh Wong <danielsenhwong@gmail.com>
2016-03-05 11:55:47 -0500

    iCal file now contains a calendar name and description

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -16,7 +16,7 @@ def hello():
 @application.route("/calendar")
 def sackler():
 #	return Response(response=ReadRSS(), mimetype='text/calendar')
-	return Response(response=("The calendar script is scheduled to run every day at 4 AM and generate a new iCal file accessible at http://sackler.danielsenhwong.com/calendar.ics\n\nAccessing this page generated a new iCal file, which is presented below in plain text.", ReadRSS()), mimetype='text/plain')
+	return Response(response=("The calendar script is scheduled to run every day at 4 AM and generate a new iCal file accessible at http://sackler.danielsenhwong.com/calendar.ics\n\nAccessing this page generated a new iCal file, which is presented below in plain text.\n\n", ReadRSS()), mimetype='text/plain')
 
 if __name__ == "__main__":
 	application.run()
