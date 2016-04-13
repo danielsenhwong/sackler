c95005f064b83769d99c8c1c7127f627228c6b4d
Daniel Wong <dsw2107@gmail.com>
2016-04-13 12:38:48 -0400

    Added micro subcalendar

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -17,7 +17,7 @@ def hello():
 @application.route("/calendar/<subcalendar>")
 def sackler(subcalendar):
 #	return Response(response=ReadRSS(), mimetype='text/calendar')
-	return Response(response=("The calendar script is scheduled to run every day at 4 AM and generate a new iCal file accessible at http://sackler.danielsenhwong.com/calendar.ics and other sub-calendars like calendar_micro.ics\n\nAccessing this page generated a new iCal file, which is presented below in plain text.\n\n", ReadRSS(subcalendar), mimetype='text/plain')
+	return Response(response=("The calendar script is scheduled to run every day at 4 AM and generate a new iCal file accessible at http://sackler.danielsenhwong.com/calendar.ics and other sub-calendars like calendar_micro.ics\n\nAccessing this page generated a new iCal file, which is presented below in plain text.\n\n", ReadRSS(subcalendar), mimetype='text/plain'))
 
 if __name__ == "__main__":
 	application.run()
